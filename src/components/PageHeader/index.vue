51944914d90b355b11fbb69d4aa2c73d7c00858c
zachcb <zach.c.barnes@gmail.com>
2019-12-28 11:21:28 -0700

    Move to tailwind styles for header; remove slime

---
+++ src/components/PageHeader/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="h-32 text-primary">
+    <div class="content flex max-w-6xl h-full mx-auto justify-between items-center">
+      <g-link class="logo px-2 text-6xl font-black uppercase italic leading-loose" href="/">
+        <span class="block">ZA</span>
+        <span class="block">CH</span>
+      </g-link>
+
+      <nav class="nav text-3xl font-black uppercase">
+        <g-link class="inline-block px-4" href="/portfolio">
+          <span class="border-transparent border-b-4">Portfolio</span>
+        </g-link>
+        <g-link class="inline-block px-4" href="/blog">
+          <span class="border-transparent border-b-4">Blog</span>
+        </g-link>
+        <g-link class="inline-block px-4" href="/resume">
+          <span class="border-transparent border-b-4">Resume</span>
+        </g-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style lang="postcss" scoped>
+.logo {
+	line-height: 3rem;
+
+	& span {
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+	}
+}
+
+.nav span {
+  transition: border-color 0.3s;
+}
+
+.nav a {
+  &:hover span {
+    border-color: var(--color-primary);
+  }
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,24 +1,6 @@
 <template>
-  <div class="page max-w-lg mx-auto">
-    <header class="page-header">
-      <div class="page-header-content">
-        <div class="logo">
-          <span>
-            ZA
-          </span>
-          <span>
-            CH
-          </span>
-        </div>
-        <nav class="page-header-nav">
-          <g-link class="nav-item" href="/portfolio">Portfolio</g-link>
-          <g-link class="nav-item" href="/blog">Blog</g-link>
-          <g-link class="nav-item" href="/resume">Resume</g-link>
-        </nav>
-      </div>
-    </header>
-
-    <slime-canvas></slime-canvas>
+  <div class="page max-w-full mx-auto">
+    <page-header></page-header>
 
     <slot/>
   </div>
@@ -33,22 +15,18 @@ query {
 </static-query>
 
 <script>
-import SlimeCanvas from "../components/SlimeCanvas";
+import PageHeader from "../components/PageHeader";
 
 export default {
   name: "DefaultLayout",
 
   components: {
-    SlimeCanvas
+    PageHeader
   }
 }
 </script>
 
 <style lang="postcss">
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
 body {
   font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   margin:0;
@@ -75,46 +53,5 @@ a {
 	color: #0E0E0E;
 	background-color: #1e272e;
 	height: 100%;
-}
-
-.page-header {
-	background-color: transparent;
-
-	& * {
-		z-index: 1000;
-	}
-}
-
-.page-header-content {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-
-	height: 100px;
-	max-width: 1200px;
-	margin: 0 auto;
-}
-
-.nav-item {
-	padding: 0px 15px;
-	font-size: 24px;
-	font-weight: bold;
-	text-transform: uppercase;
-	line-height: 100px;
-}
-
-.logo {
-	font-size: 4rem;
-	font-weight: black;
-	font-style: italic;
-	line-height: 3rem;
-
-  margin-top: 50px;
-	padding: 0px 15px;
-
-	& span {
-		display: block;
-    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
-	}
 }
 </style>
