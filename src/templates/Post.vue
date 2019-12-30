78c0059437b0355716a6e96ef0ad9f14de7cd74c
zachcb <zach.c.barnes@gmail.com>
2019-12-29 19:35:08 -0700

    Add styles to post

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,11 +1,15 @@
 <template>
   <Layout>
-    <div class="">
-      <h1>{{ $page.post.title }}</h1>
-      <span>{{ $page.post.date }}</span>
-    </div>
+    <header class="mb-8">
+      <h1 class="text-primary text-4xl font-black">
+        {{ $page.post.title }}
+      </h1>
+      <span class="text-secondary text-lg">{{ $page.post.date }}</span>
+    </header>
 
-    <div class="article__content" v-html="$page.post.content" />
+    <hr>
+
+    <div class="content text-secondary text-lg" v-html="$page.post.content" />
   </Layout>
 </template>
 
@@ -28,3 +32,33 @@
     }
   }
 </page-query>
+
+<style lang="postcss" scoped>
+.header {
+
+}
+
+.content {
+  font-family: 'Karla', sans-serif;
+
+  & >>> code {
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+  }
+
+  & >>> h2 {
+    font-size: responsive 16px 24px;
+    font-weight: bold;
+    line-height: 3rem;
+    margin-top: 2rem;
+  }
+
+  & >>> p {
+    font-size: responsive 12px 18px;
+    font-weight: normal;
+    line-height: 1.75rem;
+    max-width: 800px;
+    margin-bottom: 0.75rem;
+  }
+}
+</style>
