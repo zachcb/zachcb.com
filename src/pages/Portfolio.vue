352228dcc9d602a97ecb0e9102b32d7143978569
zachcb <zach.c.barnes@gmail.com>
2019-12-30 18:27:45 -0700

    Add star field to portfolio page

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,13 +1,27 @@
 <template>
   <Layout>
-    <h1>Portfolio</h1>
+    <template v-slot:background>
+      <star-field class="canvas"></star-field>
+    </template>
   </Layout>
 </template>
 
 <script>
+import StarField from "../components/StarField";
+
 export default {
   metaInfo: {
     title: 'Portfolio'
+  },
+
+  components: {
+    StarField
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+.canvas {
+  z-index: -1;
+}
+</style>
