36d5c04e805b516430ae9739a081f60589396135
zachcb <zach.c.barnes@gmail.com>
2019-12-30 18:26:55 -0700

    Style 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,13 +1,20 @@
 <template>
   <Layout>
-    <p>404</p>
+    <div class="content-full-height text-center mx-auto flex justify-center items-center flex-col">
+      <h1 class="text-6xl font-black tracking-widest text-primary">404</h1>
+      <p class="text-lg font-bold text-secondary">You seem to be lost, dork.</p>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Blog'
+    title: '404'
   }
 }
 </script>
+
+<style lang="postcss">
+
+</style>
