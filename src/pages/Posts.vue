d945554fcbcb82e10fa20ac77651cdddb06c610b
zachcb <zach.c.barnes@gmail.com>
2019-12-30 18:27:54 -0700

    Style posts

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,13 +1,29 @@
 <template>
   <Layout>
     <ul>
-      <li v-for="{ node } in $page.posts.edges" :key="node.id" class="text-primary mb-5 hover:bg-gray-800">
-        <g-link :to="`/posts/${node.slug}`">
-          <span class="text-secondary text-base">{{ node.date }}</span>
-          <h2 class="text-primary text-3xl font-black tracking-wider">{{ node.title }}</h2>
-          <p class="text-secondary text-lg">{{ node.description }}</p>
-        </g-link>
-      </li>
+      <template v-for="({ node }, index) in $page.posts.edges">
+        <li
+          :key="node.id"
+          :class="[{
+            'border-b-2 border-gray-700 pb-4': index + 1 < $page.posts.edges.length
+          }, 'text-primary mb-5']"
+        >
+          <g-link :to="`/posts/${node.slug}`">
+            <span class="text-secondary text-base opacity-75">{{ node.date }}</span>
+            <h2 class="text-primary text-3xl font-black tracking-wider">{{ node.title }}</h2>
+            <p class="text-secondary text-lg">{{ node.description }}</p>
+            <p class="text-secondary text-sm opacity-75">
+              <span
+                v-for="(tag, index) of node.tags"
+                :key="tag"
+                class="pr-1"
+              >
+                #{{ tag }}<span class="pl-2">{{ index + 1 < node.tags.length ? '&#8226' : '' }}</span>
+              </span>
+            </p>
+          </g-link>
+        </li>
+      </template>
     </ul>
   </Layout>
 </template>
@@ -29,6 +45,7 @@ query Post {
         date (format: "DD MMMM, YYYY")
         slug
         description
+        tags
         content
       }
     }
